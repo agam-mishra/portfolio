102a5c22766b5e966b10ef8de6e0cfd716f2899b
agam-mishra <agammishra05@gmail.com>
2024-12-10 14:25:33 +0530

    removed gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-const repoName = 'portfolio'; // Replace with your repository name
-
 const nextConfig = {
 	images: {
 		domains: ['cdn.prod.website-files.com', 'assets-global.website-files.com']
@@ -11,9 +8,6 @@ const nextConfig = {
 		'experience-bg': "url('public/tech.webp')",
 		'footer-texture': "url('/img/footer-texture.png')"
 	},
-	output: 'export',
-	assetPrefix: isGithubPages ? `/${repoName}/` : '',
-	trailingSlash: true,
 };
 
 export default nextConfig;
